8ebc28f6a415a58322e61202ebf92b357d4ed77d
Peter Bjorklund <piot@hotmail.com>
2019-06-30 19:21:50 +0200

    fix: update path in compile

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,1 +1,1 @@
-clang -Wall -Weverything -pedantic -Wno-unused-parameter -Wno-cast-align ../src/lib/*.c ../deps/tiny-libc/src/lib/*.c  -I ../src/include/ -I ../deps/include -shared
+clang -Wall -Weverything -pedantic -Wno-unused-parameter -Wno-cast-align ../src/lib/*.c   -I ../src/include/ -I ../deps/include  -shared
